b9c43c5fc0a10c697eac96c72b166edbf686916c
Anton Matosov <anton.matosov@gmail.com>
2015-12-09 00:26:46 -0800

    Added all original pod files

---
+++ Target Support Files/Pods-StylingKit_Example/Pods-StylingKit_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_StylingKit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_StylingKit_ExampleVersionString[];
+
---
+++ Target Support Files/StylingKit/StylingKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double StylingKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char StylingKitVersionString[];
+
