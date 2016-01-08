9167e5e977ffcd40e522cac02381d0ea768ee9f0
Anton Matosov <anton.matosov@gmail.com>
2016-01-08 02:48:08 -0800

    More restructuring

--- Target Support Files/StylingKit/StylingKit-umbrella.h
+++ Target Support Files/StylingKit/StylingKit-umbrella.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
 
 #import "Cloud.h"
+#import "StylingKit.h"
 
 FOUNDATION_EXPORT double StylingKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char StylingKitVersionString[];
