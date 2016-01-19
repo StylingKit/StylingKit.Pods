fa9b93b4e21bce73db46de37a8c3082a4f20dba5
Anton Matosov <anton.matosov@gmail.com>
2016-01-18 23:02:20 -0800

    pod install

---
+++ Headers/Private/StylingKit/STKThemesRegistry.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/Main/STKThemesRegistry.h
--- Target Support Files/StylingKit/StylingKit-umbrella.h
+++ Target Support Files/StylingKit/StylingKit-umbrella.h
@@ -2,6 +2,7 @@
 
 #import "STKCloud.h"
 #import "STKTheme.h"
+#import "STKThemesRegistry.h"
 #import "StylingKit.h"
 
 FOUNDATION_EXPORT double StylingKitVersionNumber;
